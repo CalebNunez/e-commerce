1874f2b8759db4718cf60b89558a07ee45364514
Caleb Nunez <calebdnunez@gmail.com>
2019-11-22 11:34:05 -0500

    Added stripe integration. Updated title/icon

---
+++ src/components/stripe-button/stripeButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_EWQ3CVv95xRrzcU6FQa6YQZI009PVFt3yr';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='E-commerce Site'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+};
+
+export default StripeCheckoutButton;
