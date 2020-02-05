8447d338486d65fa3f09304257a28f5479258a34
Caleb Nunez <calebdnunez@gmail.com>
2020-02-05 15:31:36 -0500

    Added express server to manage stripe payments.

--- client/src/components/stripe-button/stripeButton.js
+++ client/src/components/stripe-button/stripeButton.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import StripeCheckout from "react-stripe-checkout";
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_EWQ3CVv95xRrzcU6FQa6YQZI009PVFt3yr';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({ url: 'payment', method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert('There was an issue with your payment. Please be sure to use the provided credit card number.');
+    });
   };
 
   return (
